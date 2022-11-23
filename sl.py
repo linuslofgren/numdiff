eb1c069bf66627add7602a5db85d933c1d76705e
Linus von Ekensteen Löfgren <pnl.lofgren@gmail.com>
2022-11-23 18:22:00 +0100

    plot

--- sl.py
+++ sl.py
@@ -1,34 +1,46 @@
 from twopBVP import twopBVP
 from scipy import sparse
 from scipy.linalg import toeplitz, inv
-from scipy.linalg import eigh
+from scipy.linalg import eig
 import scipy.linalg as linalg
 import numpy as np
 import matplotlib.pyplot as plt
 
 def SL(N, L, alpha, beta):
-    delta_x = L/(N+1)
+    delta_x = L/(N)
     delta_x_squared = delta_x ** 2
     T = np.array(toeplitz([-2, 1]+[0]*(N-2), [-2, 1] + [0]*(N-2)))      #gör en rad kortare och ändra där nere också
-    print(T)
-    t1 = np.array((N-2)*[0]+[2/3, -2/3])
+    # print(T)
+    t1 = np.array((N-2)*[0]+[2, -2])
     T = T[:-1, :]
     T = np.concatenate((T, [t1]))
-    print(T)
+    # print(T)
     T = (1/delta_x_squared) * T
-    l, u = eigh(T)
-    l = np.flip(l)
-    u = np.flip(u)
+    l, u = eig(T)
+    res = np.flip(sorted(zip(l, u)))
     #bc = np.array([-alpha] + [0]*(N-2) + [-beta])
-    return l, u
+    return res
 
-eigen_values, eigen_vectors = SL(499 , 1, 0, 0)
-print(len(eigen_values))
-x = np.linspace(0, 1, 499)
-print(eigen_values[0:3])
-plt.plot(x, eigen_vectors[1], "2")
-#plt.plot(x, eigen_vectors[0])
-l = -4*500**2*np.sin(np.pi/(2*500))**2
-#print(l, np.max(eigen_values), eigen_values[0])
-#plt.plot(x, l, '2')
+errors = []
+errors2 = []
+errors3 = []
+for N in range(10, 100):
+    # print(res[0][0])
+    res = SL(N , 1, 0, 0)
+    # plt.plot(np.linspace(0, 1, 10), res[1][0], "2")
+    def l_eig(j, L=1):
+        return -((2*j-1)**2)*(np.pi**2)/(4*L**2)
+    # plt.show()
+    errors.append(np.sqrt(1/(N+1))*linalg.norm(l_eig(1)-res[0][1]))
+    errors2.append(np.sqrt(1/(N+1))*linalg.norm(l_eig(2)-res[1][1]))
+    errors3.append(np.sqrt(1/(N+1))*linalg.norm(l_eig(3)-res[2][1]))
+    # print("First three", res[0:3, 1])
+
+    lambdaexact = [l_eig(1), l_eig(2), l_eig(3)]
+    # print(lambdaexact)
+plt.loglog([x for x in range(10, 100)], errors, "2")
+plt.loglog([x for x in range(10, 100)], errors2, "3")
+plt.loglog([x for x in range(10, 100)], errors3, "4")
+plt.loglog([x for x in range(10, 100)], [(x)**-2 for x in range(10, 100)])
+plt.loglog([x for x in range(10, 100)], [(x)**-3 for x in range(10, 100)])
 plt.show()
