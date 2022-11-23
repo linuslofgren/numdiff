b6eb030beffd314f9db022e38b1f782dc729a03c
ebbawennerklint <ebba.wennerklint@gmail.com>
2022-11-23 13:35:49 +0100

    rms

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ def test_error_sin():
     error = []
     for N in range(10, 100):
         x = np.linspace(0, L, N+2)
+        delta_x = L/(N+1)
         y_2 = -np.sin(x)
         alpha = 0
         beta = 0
         res = [alpha]+list(twopBVP(y_2[1:-1], 0, 0, L, N))+[beta]
-        error += [linalg.norm(np.sin(x) - res)]
+        error += [np.sqrt(delta_x)*linalg.norm(np.sin(x) - res)]
         
     plt.loglog([L/(N+1) for N in range(10, 100)], error)
     plt.loglog([L/(N+1) for N in range(10, 100)], [(L/(N+1))**2 for N in range(10, 100)])
