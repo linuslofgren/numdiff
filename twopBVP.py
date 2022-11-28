45fdddb246521e88b5fcefa9c895c6a8c2302e7b
Linus von Ekensteen Löfgren <pnl.lofgren@gmail.com>
2022-11-28 11:12:15 +0100

    remove

--- twopBVP.py
+++ twopBVP.py
@@ -9,7 +9,6 @@ def twopBVP(f_vec, alpha, beta, L, N):
     delta_x_squared = delta_x ** 2
     T_times_delta_x = sparse.csr_matrix(toeplitz([-2, 1]+[0]*(N-2), [-2, 1] + [0]*(N-2)))
     bc = [-alpha] + [0]*(N-2) + [-beta]
-    # Y = (inv(T_times_delta_x) @ (f_vec*delta_x_squared + bc))
     Y = spsolve(T_times_delta_x, (f_vec*delta_x_squared + bc))
     return Y
     
